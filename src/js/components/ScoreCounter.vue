cb356ee1b98105f50aff503f7c5804c09ad19155
Vik Vanderlinden <vik.vanderlinden@hotmail.com>
2021-08-06 23:29:53 +0000

    Updated counter to show users and scores

--- src/js/components/ScoreCounter.vue
+++ src/js/components/ScoreCounter.vue
@@ -1,44 +1,35 @@
 <template>
-    <div class="max-w-sm w-full mx-auto">
-        <div class="flex">
-            <div class="text-8xl flex items-center">
-                132
+    <div class="max-w-sm w-full mx-auto p-2">
+        <div class="flex items-end" v-if="game.can_start_game()">
+            <div class="flex flex-col justify-end items-center w-2/5 flex-grow-0 flex-shrink-0">
+                <div class="text-8xl">{{game.users[game.current_user].score}}</div>
+                <div class="text-6xl">{{game.users[game.current_user].score - game.current_play_score()}}</div>
+                <div class="text-blue-400">Now: {{game.users[game.current_user].name}}</div>
             </div>
-            <div class="w-full ml-4 my-auto text-xl">
-                <div class="flex justify-between items-baseline">
-                    <div class="flex justify-start items-baseline">
-                        <div class="bg-blue-500 w-4 h-4"></div>
-                        <div class="rounded-full w-4 h-4 ml-1"></div>
-                        <div class="ml-2">Player 1</div>
-                    </div>
-                    <div>132</div>
-                </div>
-                <div class="flex justify-between">
-                    <div class="flex justify-start items-baseline">
-                        <div class="w-4 h-4"></div>
-                        <div class="bg-green-500 rounded-full w-4 h-4 ml-1"></div>
-                        <div class="ml-2">Player 2</div>
-                    </div>
-                    <div>98</div>
-                </div>
-                <div class="flex justify-between">
-                    <div class="flex justify-start items-baseline">
-                        <div class="w-4 h-4"></div>
-                        <div class="rounded-full w-4 h-4 ml-1"></div>
-                        <div class="ml-2">Player 3</div>
-                    </div>
-                    <div>124</div>
-                </div>
+            <div class="w-full ml-4 text-xl w-3/5">
+                <table class="w-full">
+                    <tr
+                        v-for="(user,index) in game.users"
+                        :class="{'text-blue-400': index == game.current_user,
+                                'text-green-400': index == game.winning_user}">
+                        <td><div v-if="index == game.current_user" class="bg-blue-500 rounded w-4 h-4 text-xs text-center text-white" title="Current">C</div></td>
+                        <td><div v-if="index == game.winning_user" class="bg-green-500 rounded w-4 h-4 text-xs text-center text-white" title="Winning">W</div></td>
+                        <td>{{user.name}}</td>
+                        <td class="text-right">{{user.score}}</td>
+                    </tr>
+                </table>
+                <div class="mt-3 text-center text-base text-blue-400">Round {{game.round}}</div>
             </div>
         </div>
-        <div class="mt-4 text-3xl text-center">3x12 + 3x20 + 3x14 = 138</div>
+        <div class="mt-4 text-3xl text-center" v-if="game.can_start_game()">{{game.current_play_calculation()}}</div>
+        <div v-else class="text-3xl text-center">Add users to start a game</div>
     </div>
 </template>
 
 <script>
 export default {
     name: "score-counter-component",
-    props: [""],
+    props: ["game"],
 }
 </script>
 
